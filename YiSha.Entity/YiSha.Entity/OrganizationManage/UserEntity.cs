a605d5af8264b6a59555332a1f9ab51016ad2b20
liukuo <liukuo.lk@163.com>
2019-11-02 11:12:35 +0800

    修改用户信息，性别覆盖

--- YiSha.Entity/YiSha.Entity/OrganizationManage/UserEntity.cs
+++ YiSha.Entity/YiSha.Entity/OrganizationManage/UserEntity.cs
@@ -17,7 +17,7 @@ namespace YiSha.Entity.OrganizationManage
         [JsonIgnore]
         public string Salt { get; set; }
         public string RealName { get; set; }
-        public int Gender { get; set; }
+        public int? Gender { get; set; }
         public string Birthday { get; set; }
         public string Portrait { get; set; }
         public string Email { get; set; }
