23a45895f6eec42155173df4af8f776b3602820f
Luke Liu <liukuo.lk@163.com>
2019-11-08 18:05:59 +0800

    增加部分前端组件示例

---
+++ YiSha.Web/YiSha.Admin.Web/Areas/DemoManage/Controllers/FormController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YiSha.Admin.Web.Areas.DemoManage.Controllers
+{
+    [Area("DemoManage")]
+    public class FormController : Controller
+    {
+        public IActionResult Button()
+        {
+            return View();
+        }
+
+        public IActionResult Grid()
+        {
+            return View();
+        }
+
+        public IActionResult Select()
+        {
+            return View();
+        }
+
+        public IActionResult Timeline()
+        {
+            return View();
+        }
+
+        public IActionResult Basic()
+        {
+            return View();
+        }
+
+        public IActionResult Cards()
+        {
+            return View();
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
