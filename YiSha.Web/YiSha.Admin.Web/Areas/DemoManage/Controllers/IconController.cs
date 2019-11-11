50713cdcfbf8f59bd9c0ff111d87c583f7539038
Luke Liu <liukuo.lk@163.com>
2019-11-11 11:15:23 +0800

    增加FontAwesome

---
+++ YiSha.Web/YiSha.Admin.Web/Areas/DemoManage/Controllers/IconController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YiSha.Admin.Web.Areas.DemoManage.Controllers
+{
+    [Area("DemoManage")]
+    public class IconController : Controller
+    {
+        public IActionResult FontAwesome()
+        {
+            return View();
+        }
+    }
+}
