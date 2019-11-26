568cbc1c21703dc6d7ca15057b23f92e3886fe30
Luke Liu <liukuo.lk@163.com>
2019-11-26 14:16:51 +0800

    增加Report示例

---
+++ YiSha.Web/YiSha.Admin.Web/Areas/DemoManage/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YiSha.Admin.Web.Areas.DemoManage.Controllers
+{
+    [Area("DemoManage")]
+    public class ReportController : Controller
+    {
+        public IActionResult ECharts()
+        {
+            return View();
+        }
+
+        public IActionResult Peity()
+        {
+            return View();
+        }
+    }
+}
