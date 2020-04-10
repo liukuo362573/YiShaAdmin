243b14c6febeafa7d0da9cb1798706bd16f4383f
Luke Liu <liukuo.lk@163.com>
2020-04-10 09:48:23 +0800

    Update UserController.cs

--- YiSha.Web/YiSha.Admin.WebApi/Controllers/UserController.cs
+++ YiSha.Web/YiSha.Admin.WebApi/Controllers/UserController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Senparc.Weixin.WxOpen.AdvancedAPIs.Sns;
 using YiSha.Business.OrganizationManage;
 using YiSha.Entity.OrganizationManage;
 using YiSha.Enum;
