ad6a9238661ef308bf936ab473d8a226b1583e26
cai.fengyuan <cai.fengyuan@ruanyun.net>
2021-05-08 00:01:21 +0800

    添加查询的单元测试

--- YiSha.Test/YiSha.DataTest/DatabaseExtensionTest.cs
+++ YiSha.Test/YiSha.DataTest/DatabaseExtensionTest.cs
@@ -1,12 +1,15 @@
 ﻿using NUnit.Framework;
-using System.Collections.Generic;
+using System;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using YiSha.Data.Extension;
 using YiSha.Data.Repository;
 using YiSha.Entity.SystemManage;
 using YiSha.Model.Param.SystemManage;
 using YiSha.Service.SystemManage;
+using YiSha.Util.Extension;
 using YiSha.Util.Model;
 
 namespace YiSha.DataTest
@@ -16,54 +19,95 @@ namespace YiSha.DataTest
         /// <summary>
         /// 测试单字段排序
         /// </summary>
-        /// <returns></returns>
         [Test]
         public async Task SortTest()
         {
-            RoleService roleService = new RoleService();
-            RoleListParam roleListParam = new RoleListParam { };
-            Pagination pagination = new Pagination
+            var roleService = new RoleService();
+            var roleListParam = new RoleListParam();
+            var pagination = new Pagination
             {
                 Sort = "RoleSort asc"
             };
-            List<RoleEntity> list = await roleService.GetPageList(roleListParam, pagination);
+            var list = await roleService.GetPageList(roleListParam, pagination);
             Assert.IsTrue(list[0].RoleSort < list[1].RoleSort);
         }
 
         /// <summary>
         /// 测试多字段排序
         /// </summary>
-        /// <returns></returns>
         [Test]
         public async Task MultiSortTest()
         {
-            RoleService roleService = new RoleService();
-            RoleListParam roleListParam = new RoleListParam { };
-            Pagination pagination = new Pagination
+            var roleService = new RoleService();
+            var roleListParam = new RoleListParam();
+            var pagination = new Pagination
             {
                 Sort = "Id desc,RoleSort asc"
             };
-            List<RoleEntity> list = await roleService.GetPageList(roleListParam, pagination);
+            var list = await roleService.GetPageList(roleListParam, pagination);
             Assert.IsTrue(list[0].Id > list[1].Id);
         }
 
         /// <summary>
         /// 测试如何获取Linq查询时对应的Sql
         /// </summary>
-        /// <returns></returns>
         [Test]
-        public async Task LinqGetSqlTest()
+        public void LinqGetSqlTest()
         {
-            string sort = "RoleSort";
-            bool isAsc = false;
-            int pageSize = 10;
-            int pageIndex = 1;
-            RepositoryFactory repositoryFactory = new RepositoryFactory();
+            var sort = "RoleSort";
+            var isAsc = false;
+            var pageSize = 10;
+            var pageIndex = 1;
+            var repositoryFactory = new RepositoryFactory();
             var tempData = repositoryFactory.BaseRepository().Db.DbContext.Set<RoleEntity>().AsQueryable();
             tempData = tempData.AppendSort(sort, isAsc);
             tempData = tempData.Skip(pageSize * (pageIndex - 1)).Take(pageSize).AsQueryable();
-            string strSql = tempData.GetSql();
-            Assert.IsTrue(strSql.ToUpper().Contains("SELECT"));
+            var sql = tempData.GetSql();
+            Assert.IsTrue(sql.ToUpper().Contains("SELECT"));
+        }
+
+        [Test]
+        public void IsElementryTypeTest()
+        {
+            Assert.IsTrue(typeof(string).IsElementaryType());
+            Assert.IsTrue(typeof(int).IsElementaryType());
+            Assert.IsTrue(typeof(decimal).IsElementaryType());
+            Assert.IsTrue(typeof(float).IsElementaryType());
+            Assert.IsTrue(typeof(StringComparison).IsElementaryType()); // enum
+            Assert.IsTrue(typeof(int?).IsElementaryType());
+            Assert.IsTrue(typeof(decimal?).IsElementaryType());
+            Assert.IsTrue(typeof(StringComparison?).IsElementaryType());
+            Assert.IsFalse(typeof(object).IsElementaryType());
+            Assert.IsFalse(typeof(Point).IsElementaryType()); // struct in System.Drawing
+            Assert.IsFalse(typeof(Point?).IsElementaryType());
+            Assert.IsFalse(typeof(StringBuilder).IsElementaryType()); // reference type
+        }
+
+        [Test]
+        public async Task GetElementryTypeValueTest()
+        {
+            var repositoryFactory = new RepositoryFactory();
+            var sql = @"select UserName from SysUser where Id = @Id";
+            var result = await repositoryFactory.BaseRepository().FindEntity<string>(sql, DbParameterHelper.CreateDbParameter("@Id", 16508640061130151));
+            Assert.IsTrue(result == "admin");
+        }
+
+        [Test]
+        public async Task GetElementryTypeValueWhenIsDbNullTest()
+        {
+            var repositoryFactory = new RepositoryFactory();
+            var sql = @"select UserName from SysUser where Id = @Id";
+            var result = await repositoryFactory.BaseRepository().FindEntity<string>(sql, DbParameterHelper.CreateDbParameter("@Id", 345));
+            Assert.IsTrue(result == default);
+        }
+
+        [Test]
+        public async Task GetDynamicValueTest()
+        {
+            var repositoryFactory = new RepositoryFactory();
+            var sql = @"select UserName from SysUser where Id = @Id";
+            var result = await repositoryFactory.BaseRepository().FindEntity<dynamic>(sql, DbParameterHelper.CreateDbParameter("@Id", 16508640061130151));
+            Assert.IsTrue(result.UserName == "admin");
         }
     }
 }
